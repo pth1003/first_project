644bc7a35f65ba61d1ad7af64ece8a24264f5997
pthau1003 <pthau@ninepoints.vn>
2023-02-08 12:00:40 +0700

    Update login register

--- post_list.php
+++ post_list.php
@@ -40,7 +40,31 @@
                         echo ('<h1 style="text-align: center; margin-top: 20px">Chưa có bài đăng nào</h1>');
                     }
                     while($rowList = mysqli_fetch_array($sqlList)) {
-                        if($rowList['is_private'] == 0) { ?>
+                        if($_SESSION['user_id'] == $rowList['user_id']){ ?>
+                            <div class="post-items">
+                                <a href="./post_detail.php?post_id=<?php echo $rowList['post_id'] ?>"><img src="./img/img_post/<?php echo $rowList['image_url'] ?>" alt="" class="post-items-img"></a>
+                                <div>
+                                    <p class="post-items-title"><a href="./post_detail.php?post_id=<?php echo $rowList['post_id'] ?>"><?php echo $rowList['title'] ?></a></p>
+                                    <span class="post-items-user"><?php echo $rowList['fullname'] ?></span><span> -</span>
+                                    <span class="post-items-time"><?php echo $rowList['post_date'] ?></span>
+                                    <span style="color: #ea9920" class="post-items-time">
+                                        <?php
+                                        if($rowList['is_private'] == 0){
+                                            echo ('Công Khai');
+                                        }else{
+                                            echo ('Riêng Tư');
+                                        }
+                                        ?>
+                                    </span>
+                                    <p class="post-items-description">
+                                        <?php $content =  $rowList['content'];
+                                        echo substr($content,0,400);?>
+                                        <a href="./post_detail.php?post_id=<?php echo $rowList['post_id'] ?>" href="post_detail.php?post_id=<?php echo $rowList['post_id'] ?>"> ...xem thêm</a>
+                                    </p>
+                                </div>
+                            </div>
+                        <?php }
+                        elseif($rowList['is_private'] == 0) { ?>
                             <div class="post-items">
                                 <a href="./post_detail.php?post_id=<?php echo $rowList['post_id'] ?>"><img src="./img/img_post/<?php echo $rowList['image_url'] ?>" alt="" class="post-items-img"></a>
                                 <div>
