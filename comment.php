d7423a6824a3e83dceb9620d196be43876710afa
pthau1003 <pthau@ninepoints.vn>
2023-02-12 22:45:21 +0700

    update cmt

--- comment.php
+++ comment.php
@@ -5,14 +5,19 @@
     if(isset($_POST['sbm-cmt'])) {
         $cmt_content = $_POST['cmt_content'];
         $user_id = $_SESSION['user_id'];
-        $sqlCmt = mysqli_query($conn, "INSERT into comment(cmt_content, user_id, post_id) values ('$cmt_content', $user_id, $post_id )");
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $cmt_date = date('Y-m-d h:i:s');
+        $sqlCmt = mysqli_query($conn, "INSERT into comment(cmt_content, user_id, post_id, date_cmt) values ('$cmt_content', $user_id, $post_id, '$cmt_date')");
 
         if($sqlCmt){
             echo "   <script type='text/javascript'>alert('Thành công');
                     window.location = history.go(-1)
                     </script>";
         }else{
-            echo "<script type='text/javascript'>alert('Thất bại');</script>";
+            echo "<script type='text/javascript'>alert('Thất bại');
+                    window.location = history.go(-1)
+</script>";
+
         }
     }
 ?>
